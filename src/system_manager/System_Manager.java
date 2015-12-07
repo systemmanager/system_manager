d5e2e4851a03c197bc421ae48fa7a1cd3fc09558
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2015-12-08 04:19:40 +0530

    project start. create System_manager.java run it. no error. 100% OK

---
+++ src/system_manager/System_Manager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package system_manager;
+
+/**
+ *
+ * @author deepal_suranga
+ */
+public class System_Manager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to System!");
+        // TODO code application logic here
+    }
+    
+}
